369340a81de7541c335a82209b48113930b8827a
khadimmbaye0 <khadimmbaye4002@gmail.com>
2026-07-26 21:42:18 +0000

    (conf): More springdoc openapi config

---
+++ src/main/java/com/example/demo/config/OpenApiConfig.java
@@ -0,0 +1,21 @@
+package com.example.demo.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(info = @Info(
+        title = "SénSanté Pro API",
+        version = "v0.4",
+        description = "API REST de la plateforme de santé communautaire SénSanté Pro"
+))
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT"
+)
+public class OpenApiConfig {}
