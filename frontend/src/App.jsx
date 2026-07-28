0274d6baff13c9acd5d82a12fb19a95d54c0cb85
khadimmbaye0 <khadimmbaye4002@gmail.com>
2026-07-28 20:45:08 +0000

    (feat): App.jsx protected

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import { Routes, Route, Navigate } from "react-router-dom";
+import RouteProtegee from "./components/RouteProtegee";
 import LoginPage from "./pages/LoginPage";
 import PatientsPage from "./pages/PatientsPage";
 import ConsultationsPage from "./pages/ConsultationsPage";
@@ -7,12 +8,17 @@ import DashboardPage from "./pages/DashboardPage";
 function App() {
   return (
     <Routes>
-      {/* Route par défaut -> login */}
       <Route path="/" element={<Navigate to="/login" />} />
       <Route path="/login" element={<LoginPage />} />
-      <Route path="/patients" element={<PatientsPage />} />
-      <Route path="/consultations" element={<ConsultationsPage />} />
-      <Route path="/dashboard" element={<DashboardPage />} />
+      <Route path="/patients" element={
+        <RouteProtegee><PatientsPage /></RouteProtegee>
+      } />
+      <Route path="/consultations" element={
+        <RouteProtegee><ConsultationsPage /></RouteProtegee>
+      } />
+      <Route path="/dashboard" element={
+        <RouteProtegee><DashboardPage /></RouteProtegee>
+      } />
     </Routes>
   );
 }
