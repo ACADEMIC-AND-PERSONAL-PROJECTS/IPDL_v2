aaa074978c3b1e4f35176387ac26df1c833adea9
khadimmbaye0 <khadimmbaye4002@gmail.com>
2026-07-28 15:35:53 +0000

    (feat): Ai Service added

--- src/main/java/com/example/demo/ia/service/DiagnosticAiService.java
+++ src/main/java/com/example/demo/ia/service/DiagnosticAiService.java
@@ -1,21 +1,44 @@
 package com.example.demo.ia.service;
 
+import com.example.demo.ia.ai_exchange.DiagnosticAiResult;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class AiService {
+@Slf4j
+public class DiagnosticAiService {
 
     private final ChatClient chatClient;
 
-    private final String DISCLAIMER = """
-            Ceci n'est pas un diagnostic medical.
-            Les suggestions de l'IA sont indicatives et ne remplacent en aucun cas,
-            l'avis d'un professionnel de santé qualifié.
-            """;
+    // Analyse les symptômes et retourne un diagnostic structure
+    public DiagnosticAiResult analyserSymptomes(String symptomes, String contextPatient) {
+        String DISCLAIMER = """
+                Ceci n'est pas un diagnostic medical.
+                Les suggestions de l'IA sont indicatives et ne remplacent en aucun cas,
+                l'avis d'un professionnel de santé qualifié.
+                """;
+        var diagnosticIA = new DiagnosticAiResult("IA indisponible", 0.0, "Consulter un professionnel", DISCLAIMER);
+        try {
+            diagnosticIA = chatClient
+                    .prompt(constructUserPrompt(symptomes, contextPatient + " colle ceci pour le disclaimer: " + DISCLAIMER))
+                    .call()
+                    .entity(DiagnosticAiResult.class);
+        } catch (Exception e) {
+            log.error("AI Error: {}", e.getMessage());
+        }
 
-    public 
+        return diagnosticIA;
+    }
+
+    // Construire un prompt utilisateur bien formatter
+    private String constructUserPrompt(String symptomes, String contextPatient) {
+        return String.format(
+                "Contexte patient : %s%n%nSymptomes décrits :%s",
+                contextPatient != null ? contextPatient : "Non precisé", symptomes
+        );
+    }
 
 }
